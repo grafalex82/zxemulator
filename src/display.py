863460bcdd0f1a9a6a6f092d3ec6dd88816b00ce
Oleksandr Masliuchenko <omaslyuchenko@gmail.com>
2025-01-02 19:52:53 +0200

    Display: improve performance by updating only changed pixels or color attributes

--- src/display.py
+++ src/display.py
@@ -12,6 +12,10 @@ class Display(RAM):
         RAM.__init__(self, 0x1b00)  # Size is 256*192/8 bytes for pixels, and 256*192/(8*8) = 768 bytes for colors
         size = (DISPLAY_WIDTH * SCALE, DISPLAY_HEIGHT * SCALE)
         self._display = pygame.Surface(size)
+        self._display.fill((0, 0, 0))
+
+        # Pixmap
+        self._pixmap = [0] * (DISPLAY_WIDTH * DISPLAY_HEIGHT)
 
         # ZX spectrum color palete is using GRB colors order, and the 4th bit is used for brightness
         self._colors = [
@@ -40,30 +44,67 @@ class Display(RAM):
         self._display.set_at((x * SCALE + 1, y * SCALE + 1), color)
 
 
+    def _update_pixels(self, offset, value):
+        # Address is calculated as bit shuffle as follows:
+        # a12 a11 a10 a9  a8   a7  a6  a5  a4  a3  a2  a1  a0
+        # y7  y6  y2  y1  y0   y5  y4  y3  x4  x3  x2  x1  x0
+        # see http://www.breakintoprogram.co.uk/hardware/computers/zx-spectrum/screen-memory-layout
+        #
+        # Here we need a reverse shuffling, to get the x and y coordinates from the address
+        # X: a4 a3 a2 a1 a0
+        # Y: a12 a11 a7 a6 a5 a10 a9 a8
+        x_block = offset & 0x1f
+        y = ((offset & 0x700) >> 8) | ((offset & 0xe0) >> (5-3)) | ((offset & 0x1800) >> (11-6))
+
+        # Get color attributes for the block
+        color_addr = 0x1800 + (y // 8) * (DISPLAY_WIDTH // 8) + x_block
+        attr = self.read_byte(color_addr)
+        bg = (attr & 0x78) >> 3
+        bg_color = self._colors[bg]
+        fg = attr & 0x7 | ((attr & 0x40) >> 3)
+        fg_color = self._colors[fg]
+
+        # Update pixels in the corresponding block
+        for bit in range(8):
+            x = x_block * 8 + bit
+            mask = 1 << (7 - bit)
+            pixel = value & mask != 0
+
+            self._pixmap[y * DISPLAY_WIDTH + x] = pixel
+            self._set_pixel(x, y, fg_color if pixel else bg_color)
+
+
+    def _update_colors(self, attr_offset, value):
+        # Get coordinates of the 8x8 block
+        x_block = attr_offset % 32
+        y_block = attr_offset // 32
+        
+        # Get color attributes for the block
+        bg = (value & 0x78) >> 3
+        bg_color = self._colors[bg]
+        fg = value & 0x7 | ((value & 0x40) >> 3)
+        fg_color = self._colors[fg]
+
+        # Update pixel colors in the corresponding block
+        for x_bit in range(8):
+            x = x_block * 8 + x_bit
+            for y_bit in range(8):
+                y = y_block * 8 + y_bit
+                pixel = self._pixmap[y * DISPLAY_WIDTH + x]
+                self._set_pixel(x, y, fg_color if pixel else bg_color)
+
+
+    def write_byte(self, offset, value):
+        # Update the RAM value as usual
+        RAM.write_byte(self, offset, value)
+
+        # Then update corresponding pixels on the screen
+        # Note that offset is relative to 0x4000 video memory start
+        if offset < 0x1800:
+            self._update_pixels(offset, value)
+        else:
+            self._update_colors(offset - 0x1800, value)
+
+
     def update(self, screen):
-        for y in range(DISPLAY_HEIGHT):
-            for x_block in range(DISPLAY_WIDTH // 8):
-                # Address is calculated as bit shuffle as follows:
-                # y7 y6 y2 y1 y0  y5 y4 y3 x4 x3 x2 x1 x0
-                # see http://www.breakintoprogram.co.uk/hardware/computers/zx-spectrum/screen-memory-layout
-                addr = ((y >> 6) << 11) | (y & 0x7) << 8 | ((y >> 3) & 0x7) << 5 | x_block
-
-                # Get color attributes for the block
-                addr_color = 0x1800 + (y // 8) * (DISPLAY_WIDTH // 8) + x_block
-                attr = self.read_byte(addr_color)
-                bg = (attr & 0x78) >> 3
-                bg_color = self._colors[bg]
-                fg = attr & 0x7 | ((attr & 0x40) >> 3)
-                fg_color = self._colors[fg]
-
-                # TODO: respect blinking attribute
-
-                # Draw the block with the colors calculated
-                block_data = self.read_byte(addr)
-                for bit in range(8):
-                    x = x_block * 8 + bit
-                    mask = 1 << (7 - bit)
-                    color = fg_color if block_data & mask else bg_color
-                    self._set_pixel(x, y, color)
-
         screen.blit(self._display, (0, 0))
